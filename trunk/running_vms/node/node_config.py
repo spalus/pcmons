6c559a185c6d378abd8806e9ee036830f7c44bcb
shirlei@gmail.com <shirlei@gmail.com@48d07b76-333c-c244-a297-7618656dcc95>
2011-03-13 20:12:34 +0000

    updating...

--- trunk/running_vms/node/node_config.py
+++ trunk/running_vms/node/node_config.py
@@ -2,7 +2,7 @@
 PATH_TO_LOG_FILE = '/var/log/Monitoring_Cloud_Client.log'
 
 #Current ip of the interface listening to requests
-LISTEN_IP = '150.162.63.25'
+LISTEN_IP = 'cirrus.lrg.ufsc.br'
 
 #Port where this client will listen to requests
 LISTEN_PORT = 60000
